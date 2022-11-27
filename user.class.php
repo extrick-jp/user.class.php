7bc2255172df4e7267ccc48e5e935f8ba5774740
Yasunari Oota <y.oota@extrick.jp>
2022-11-27 10:36:30 +0900

    version 4.0
    use PDO

--- user.class.php
+++ user.class.php
@@ -8,6 +8,7 @@
     version 3.0.2 2021.10. 3 -
     version 3.1.0 2022. 7.29 -
     version 3.2   2022.10.17 -
+    version 4.0   2022.11.27 - Use PDO::
 
     Usage:
       $user = new user();
@@ -22,8 +23,16 @@ function __construct(){
     include __DIR__.'/user.config.php';
     $this->expires_time = $this->config['expires'] + time();
 
-    // DB
-    $this->db = new mysqli($this->config['db_host'], $this->config['db_user'], $this->config['db_pass'], $this->config['db_name']);
+    // DB https://www.php.net/manual/ja/pdo.drivers.php
+    switch ($this->config['db_type']){
+        case 'mysql':
+            $this->db = new PDO('mysql:host='.$this->config['db_host'].';dbname='.$this->config['db_name'], $this->config['db_user'], $this->config['db_pass']);
+            break;
+        case 'sqlite':
+            $this->db = new PDO('sqlite:'.$this->config['db_name']);
+            break;
+    }
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $this->script_name = $_SERVER['SCRIPT_NAME'];
     $this->p = array();
@@ -32,8 +41,8 @@ function __construct(){
     $this->keep_login = $this->config['keep_login'];
 
     // Delete expired sessions
-    $sql = "delete from user_session where session_expire < '".time()."'";
-    $this->db->query($sql);
+    $sql = $this->db->prepare('delete from user_session where session_expire < ?');
+    $sql->execute([time()]);
 
     // User session ID stored in COOKIE
     $this->session_u = '';
@@ -60,30 +69,30 @@ public function login(){
 
     // returned from the login form
     if (isset($_POST['loginname']) && isset($_POST['password'])){
-        $q_loginname = $this->quote($_POST['loginname']);
-        $sql = "select `userid`, `password`, `auth`, `loginname` from users where `loginname` = {$q_loginname}";
-        $rtn = $this->db->query($sql);
-        if (!$rtn->num_rows){   // Loginname or password is invalid
-            $this->logout();
-            $this->loginform('Loginname or password is invalid.');
-        }
+        $sql = $this->db->prepare('select `userid`, `password`, `auth`, `loginname` from users where `loginname` = ?');
+        $sql->execute([$_POST['loginname']]);
 
-        list($fetch_id, $hashed_password, $fetch_auth, $fetch_name) = $rtn->fetch_row();
-        $rtn->free();
+        if ($row = $sql->fetch(PDO::FETCH_NUM)){
+            list($userid, $hashed_password, $auth, $loginname) = $row;
+        }
+        else {   // 入力した loginname が不正
+            $this->logout();
+            $this->loginform('Loginname or password is invalid.');   // ログインフォームに移動してエラーメッセージを表示する
+        }
 
         /*
             $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
         */
         if (password_verify($_POST['password'], $hashed_password) || $hashed_password == md5($_POST['password'])){
-            $this->userid = $fetch_id;
-            $this->auth = $fetch_auth;
-            $this->loginname = $fetch_name;
+            $this->userid = $userid;
+            $this->auth = $auth;
+            $this->loginname = $loginname;
 
             // md5 -> bcrypt
             if ($hashed_password == md5($_POST['password'])){
                 $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-                $sql = "update users set `password` = '{$hashed_password}' where `userid` = '{$this->userid}'";
-                $this->db->query($sql);
+                $sql = $this->db->prepare('update users set `password` = ? where `userid` = ?');
+                $sql->execute([$hashed_password, $this->userid]);
             }
         }
         else {
@@ -96,31 +105,45 @@ public function login(){
 
         //
         $this->session_u = $this->make_session(32);
-        $sql = <<<_SQL_
+        $sql_sentence = <<<_SQL_
 insert into user_session (`session`, `userid`, `session_expire`, `keep_login`)
-values ('{$this->session_u}', '{$this->userid}', {$this->expires_time}, {$this->keep_login})
+values (?, ?, ?, ?)
 _SQL_;
-        $this->db->query($sql);
+        $sql = $this->db->prepare($sql_sentence);
+        $sql->execute([$this->session_u, $this->userid, $this->expires_time, $this->keep_login]);
     }
 
     // Check session stored in cookie
     else if ($this->session_u){
-        $q_session_u = $this->quote($this->session_u);
-        $sql = "select `userid`, `keep_login` from `user_session` where `session` = {$q_session_u}";
-        $rtn = $this->db->query($sql);
-        if ($rtn->num_rows){
-            list($this->userid, $this->keep_login) = $rtn->fetch_row();
-            $sql = "select `auth`, `loginname` from users where `userid` = '{$this->userid}'";
-            $rtn = $this->db->query($sql);
-            list($this->auth, $this->loginname) = $rtn->fetch_row();
-            $rtn->free();
+        $sql = $this->db->prepare('select userid, keep_login from `user_session` where `session` = ?');
+        $sql->execute([$this->session_u]);
+        if ($row = $sql->fetch(PDO::FETCH_NUM)){
+            list($this->userid, $this->keep_login) = $row;
+
+            $sql = $this->db->prepare('select `auth`, `loginname` from users where `userid` = ?');
+            $sql->execute([$this->userid]);
+            $row = $sql->fetch(PDO::FETCH_NUM);
+            list($this->auth, $this->loginname) = $row;
         }
         else {
             $this->logout();
             $this->loginform('Please login again.');
         }
-    }
 
+        // session update
+        if ($this->config['update_session']){
+            $new_session = $this->make_session(32);
+            $sql_sentence = <<<_SQL_
+update user_session
+set `session` = ?, `session_expire` = ?, `keep_login` = ?
+where `session` = ?
+_SQL_;
+            $sql = $this->db->prepare($sql_sentence);
+            $sql->execute([$new_session, $this->expires_time, $this->keep_login, $this->session_u]);
+            $this->session_u = $new_session;
+        }
+
+    }
 
     else {
         $this->logout();
@@ -145,15 +168,14 @@ _SQL_;
 
     // Single login: Delete invalid own sessions
     if (!$this->config['multi_login']){
-        $sql = "delete from `user_session` where `userid`='{$this->userid}' and `session` != '{$this->session_u}'";
-        $this->db->query($sql);
+        $sql = $this->db->prepare('delete from `user_session` where `userid` = ? and `session` != ?');
+        $sql->execute([$this->userid, $this->session_u]);
     }
 
     // get property
-    $sql = "select * from user_property where `userid` = '{$this->userid}' limit 1";
-    $rtn = $this->db->query($sql);
-    $this->p = $rtn->fetch_assoc();
-    $rtn->free();
+    $sql = $this->db->prepare('select * from user_property where `userid` = ? limit 1');
+    $sql->execute([$this->userid]);
+    $this->p = $sql->fetch(PDO::FETCH_ASSOC);
 
     return;
 }
@@ -162,8 +184,8 @@ _SQL_;
 public function logout(){
     // Delete session
     if (isset($this->session_u) && $this->session_u){
-        $sql = "delete from user_session where `session` = '{$this->session_u}'";
-        $this->db->query($sql);
+        $sql = $this->db->prepare('delete from user_session where `session` = ?');
+        $sql->execute([$this->session_u]);
     }
 
     // Delete Cookie
@@ -182,31 +204,33 @@ public function logout(){
 // Issue guest-id
 public function guestid(){
     // Delete expired guest ID
-    $sql = "delete from users where `auth` = 0 and `password` < '".time()."'";
-    $this->db->query($sql);
+    $sql = $this->db->prepare('delete from users where `auth` = 0 and `password` < ?');
+    $sql->execute([time()]);
 
     // Extend guestid expiration time
     if (isset($_COOKIE[$this->config['cookie_g']]) && $_COOKIE[$this->config['cookie_g']]){
         $g = $_COOKIE[$this->config['cookie_g']];
-        $q_g = $this->quote($g);
-        $sql = "update users set `password` = '{$this->expires_time}' where `userid` = {$g}";
+        $sql = $this->db->prepare('update users set `password` = ? where `userid` = ?');
+        $sql->execute([$this->expires_time, $g]);
     }
     // issue guestid
     else {
         $e = 1;
         while ($e){
             $g = $this->make_rndstr(32);
-            $sql = "select userid from users where userid = '{$g}'";
-            $rtn = $this->db->query($sql);
-            $e = $rtn->num_rows;
-            $rtn->free();
+            $sql = $this->db->prepare('select userid from users where userid = ?');
+            $sql->execute([$g]);
+            $e = $sql->fetch(PDO::FETCH_NUM);
         }
-        $sql = <<<_SQL_
+
+        $sql_sentence = <<<_SQL_
 insert into users (`userid`, `loginname`, `password`, `auth`)
-values ('{$g}', '{$g}', '{$this->expires_time}', 0)
+values (?, ?, ?, 0)
 _SQL_;
+        $sql = $this->db->prepare($sql_sentence);
+        $sql->execute([$g, $g, $this->expires_time, 0]);
     }
-    $this->db->query($sql);
+
     setcookie($this->config['cookie_g'], $g, $this->get_cookie_options($this->expires_time));
     $this->guestid = $g;
 
@@ -239,9 +263,9 @@ private function make_session($len){
     $e = 1;
     while ($e){
         $session = $this->make_rndstr($len);
-        $rtn = $this->db->query("select `session` from user_session where `session` = '{$session}' limit 1");
-        $e = $rtn->num_rows;
-        $rtn->free();
+        $sql = $this->db->prepare('select `session` from user_session where `session` = ? limit 1');
+        $sql->execute([$session]);
+        $e = $sql->fetch(PDO::FETCH_NUM);
     }
     return $session;
 }
@@ -253,11 +277,6 @@ private function make_rndstr($len){
     $rndstr = '';
     for ($i = 1; $i <= $len; $i++){ $rndstr .= substr($seedstr, mt_rand(0, $randmax), 1); }
     return $rndstr;
-}
-
-
-private function quote($val){
-    return "'" . $this->db->real_escape_string($val) . "'";
 }
 
 
@@ -278,30 +297,32 @@ public function useradd($loginname, $password, $auth=1){
     $e = 1;
     while($e){
         $userid = make_rndstr(32);
-        $sql = "select userid from users where userid = '{$userid}'";
-        $rtn = $this->db->query($sql);
-        $e = $rtn->num_rows();
-        $rtn->free();
+        $sql = $this->db->prepare('select userid from users where userid = ?');
+        $sql->execute([$userid]);
+        $e = $sql->fetch(PDO::FETCH_NUM);
     }
-    $q_loginname = $this->quote($loginname);
-    $q_password = $this->quote(password_hash($password, PASSWORD_BCRYPT));
-    $sql = "insert into users (`userid`, `loginname`, `password`, `auth`) values ('{$userid}', {$q_loginname}, {$q_password}, {$auth})";
-    $this->db->query($sql);
-    $sql = "insert into user_property (`userid`) values ('{$userid}')";
-    $this->db->query($sql);
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    $sql = $this->db->prepare('insert into users (`userid`, `loginname`, `password`, `auth`) values (?, ?, ?, ?)');
+    $sql->execute([$userid, $loginname, $hashed_password, $auth]);
+
+    $sql = $this->db->prepare('insert into user_property (`userid`) values (?)');
+    $sql->execute([$userid]);
     return $userid;
 }
 
 
 public function userdel($userid){
     if ($this->auth < 9){ return false; }
-    $q_userid = $this->quote($userid);
-    $sql = "delete from users where `userid` = {$q_userid}";
-    $this->db->query($sql);
-    $sql = "delete from user_property where `userid` = {$q_userid}";
-    $this->db->query($sql);
-    $sql = "delete from user_session where `userid` = {$q_userid}";
-    $this->db->query($sql);
+
+    $sql = $this->db->prepare('delete from users where `userid` = ?');
+    $sql->execute([$userid]);
+
+    $sql = $this->db->prepare('delete from user_property where `userid` = ?');
+    $sql->execute([$userid]);
+
+    $sql = $this->db->prepare('delete from user_session where `userid` = ?');
+    $sql->execute([$userid]);
+
     return $userid;
 }
 
--- user.config.php
+++ user.config.php
@@ -1,18 +1,21 @@
 <?php
 /*
-	user.config.php
+    user.config.php
+    for version 4.x
 -------------------------------------------------------------------------------*/
 $this->config = array(
     'keep_login' => 0,
     'multi_login' => 1,
+    'update_session' => 1,
 
-    'cookie_u' => 'u',
-    'cookie_g' => 'g',
-    'expires' => 3600 * 24 * 30,      // Expiry time of stored cookies
+    'cookie_u' => 'user',
+    'cookie_g' => 'guest',
+    'expires' => 3600 * 24 * 30,        // Expiry time of stored cookies
 
-    'login_url' => '/login.php',      // Login form
-    'logout_url' => 'https://hoge.jp/',  // Transition destination when logged out
+    'login_url' => '/login.php',        // Login form
+    'logout_url' => 'https://hoge.jp/', // Transition destination when logged out
 
+    'db_type' => 'PDO_INTERFACE',       // see https://www.php.net/manual/ja/pdo.drivers.php
     'db_host' => 'YOUR_DB_HOST',
     'db_user' => 'YOUR_DB_USER',
     'db_pass' => 'YOUR_DB_PASS',
